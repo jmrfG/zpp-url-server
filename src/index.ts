e2fd7d6de378efda411a4cd5dec1c6124ccb7dce
jmrfG <jorge_ramos13@outlook.com.br>
2023-02-12 18:09:52 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,17 @@ import "./lib/db";
 import express from "express";
 import urlRoutes from "./routes/url";
 
+
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3333;
-
+app.use(cors())
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+  res.json({ message: "" });
 });
 
 app.use("/urls", urlRoutes);
